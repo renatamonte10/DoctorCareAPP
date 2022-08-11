ee519ae25e6f48e23c825d6286bca33a0e41ec9b
RenataNevesSescBa <ba9184@sescbahia.com.br>
2022-08-11 11:03:20 -0300

    Aula5_AjustesDeJS+CSS

--- main.js
+++ main.js
@@ -1,16 +1,63 @@
+window.addEventListener("scroll", onScroll);
+var navigation2 = document.getElementById("navigation");
+var backToTopButton2 = document.getElementById("backToTopButton");
+
+onScroll();
 function onScroll() {
   showNavOnScroll();
   showBackToTopButtonOnScroll();
+
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(contact);
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+
+  //verificar se a seção passou da linha
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+
+  //se o topo da seção chegou ou passou da linha
+  const sectionTopReachOfPassedTargetLine = targetLine >= sectionTop;
+
+  //onde termina a seção
+  const sectionEndsAt = sectionTop + sectionHeight;
+
+  //o final da seção passou da linha alvo
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
+
+  //limites da seção
+  const sectionBoundaries =
+    sectionTopReachOfPassedTargetLine && !sectionEndPassedTargetLine;
+
+  const menuElement = document.querySelector(
+    `.menu a[href*=${section.getAttribute("id")}]`
+  );
+
+  menuElement.classList.remove("active");
+  if (sectionBoundaries) {
+    console.log("ESTOU NA SEÇÃO HOME");
+    menuElement.classList.add("active");
+  }
 }
 
 function showNavOnScroll() {
-  if (scrollY == 0) navigation.classList.remove("scroll");
-  else navigation.classList.add("scroll");
+  if (scrollY > 0) {
+    navigation2.classList.add("scroll");
+  } else {
+    navigation2.classList.remove("scroll");
+  }
 }
 
 function showBackToTopButtonOnScroll() {
-  if (scrollY > 400) backToTopButton.classList.add("show");
-  else backToTopButton.classList.remove("show");
+  if (scrollY > 550) {
+    backToTopButton2.classList.add("show");
+  } else {
+    backToTopButton2.classList.remove("show");
+  }
 }
 
 function openMenu() {
